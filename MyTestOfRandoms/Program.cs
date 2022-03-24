18fd58e63737219885c92c4364f5ead913284837
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-24 11:59:33 +0000

    Cast die using Random class

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -9,11 +9,24 @@ namespace MyTestOfRandoms
             // Variable to store user input
             int n;
 
+            // Variable to store sum of thrown die
+            int sum = 0;
+
             // Ask user for input
             Console.Write("Insert number of die to throw: ");
 
             // Store user input in variable
             n = Convert.ToInt32(Console.ReadLine());
+
+            // Start loop until 'i' is equal to 'n'
+            for (int i = 1; i <= n; i++)
+            {
+                // Initialize new variable of Random class
+                Random rnd = new Random();
+
+                // Add value of current dice to sum
+                sum += rnd.Next(1, 6);
+            }
         }
     }
 }
